afc9229e7d85d717f554de0e34ec55bac54106e4
yusuf-uthman <yusufuthman57@gmail.com>
2022-06-28 19:16:44 +0100

    loop thorough and get all api data using payload availability

---
+++ api_rick_and_morty/data_by_result.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+
+url = 'https://rickandmortyapi.com/api/character?page='
+page_num = 40
+data_results = []
+
+while True:
+    url = f'https://rickandmortyapi.com/api/character?page={page_num}'
+    response = requests.get(url)
+    data  = response.json()
+    
+    try:
+        print(len(data['results'][0]))
+    except:
+        print('No more results, Exit loop')
+        break
+    data_results.extend(data['results'])    
+    page_num +=1
+
+print(f"lenght of data is {data_results}")
+
+df_data_results = pd.DataFrame(data_results)
+print(df_data_results.head())
