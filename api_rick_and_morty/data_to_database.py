f5525e846b80dc109a4a05d6f4910cfab10fa691
yusuf-uthman <yusufuthman57@gmail.com>
2022-06-29 22:28:14 +0100

    created a function to pull nad insert data into the database

--- api_rick_and_morty/data_to_database.py
+++ api_rick_and_morty/data_to_database.py
@@ -16,57 +16,59 @@ conn   = engine.raw_connection()
 cursor = conn.cursor()
 
 
-# pull data from the api into alist as both list and dictionary
-page_num = 41 #This proces start from 40 to avoid multiple unneccessary to the API
+# function to pull and insert api data inot the database
+def pull_and_insert_data(schema, table, page_num):
+    actor_dict_list = []  #list for dicts
+    actor_list_list = []  #list for lists
 
-actor_dict_list = []
-actor_list_list = []
+    while True:
+        url = f'https://rickandmortyapi.com/api/character?page={page_num}'
+        response = requests.get(url)
+        data  = response.json()
 
-while True:
-    url = f'https://rickandmortyapi.com/api/character?page={page_num}'
-    response = requests.get(url)
-    data  = response.json()
-    
-    try:
-        print(f"This payload has {len(data['results'][0])} records")
-    except:
-        print('No more results, Exit loop')
-        break
-    for rec in data['results']:
-        #dictionary
-        actor_dict ={
-            'id': rec['id'],
-            'name': rec['name'],
-            'species': rec['species'],
-            'type': rec['type'],
-            'gender': rec['gender'],
-            'origin': rec['origin']['name'],
-            'url': rec['url'],
-            'location': rec['location']['name'],
-            'image': rec['image'],
-            'num_episode_feature': len(rec['episode']),
-            'created': rec['created']
-        }
-        #list
-        actor_list =[
-           rec['id'],
-           rec['name'],
-           rec['species'],
-           rec['type'],
-           rec['gender'],
-           rec['origin']['name'],
-           rec['url'],
-           rec['location']['name'],
-           rec['image'],
-       len(rec['episode']),
-           rec['created']
-        ]
-        actor_dict_list.append(actor_dict)
-        actor_list_list.append(actor_list)   
-    page_num +=1
-
-print(f"Actor_Dict_List has {len(actor_dict_list)} records while Actor_List_List {len(actor_list_list)} records")
-
+        try:
+            print(f"This payload has {len(data['results'][0])} records")
+        except:
+            print('No more data in API, Exited loop!')
+            break
+        for rec in data['results']:
+            #dictionary
+            actor_dict ={
+                'id': rec['id'],
+                'name': rec['name'],
+                'species': rec['species'],
+                'type': rec['type'],
+                'gender': rec['gender'],
+                'origin': rec['origin']['name'],
+                'url': rec['url'],
+                'location': rec['location']['name'],
+                'image': rec['image'],
+                'num_episode_feature': len(rec['episode']),
+                'created': rec['created']
+            }
+            #list
+            actor_list =[
+               rec['id'],
+               rec['name'],
+               rec['species'],
+               rec['type'],
+               rec['gender'],
+               rec['origin']['name'],
+               rec['url'],
+               rec['location']['name'],
+               rec['image'],
+           len(rec['episode']),
+               rec['created']
+            ]
+            actor_dict_list.append(actor_dict)
+            actor_list_list.append(actor_list)   
+        page_num +=1
+    for rec in actor_list_list:
+        cursor.execute(f"""INSERT into {schema}.{table}(id, name, species, type, gender, origin, url, location,
+                           image, num_episode_feature, created) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)  
+                        """, rec)
+        conn.commit()
+    return[actor_dict_list,actor_list_list]
 
 # create schema function
 def create_schema(schema):
