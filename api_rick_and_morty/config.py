c90fcb6d80c55dfba94327e624f4985f717ed833
Uthman Yusuf <43986317+yusuf-uthman@users.noreply.github.com>
2022-06-29 08:26:38 +0100

    Deleted cofig file with dB credentials

--- api_rick_and_morty/config.py
+++
@@ -1,5 +0,0 @@
-password = '06938df49ede973cf4716b6b2aba6a991a1f74a83eeb57b4ddc76d69001736b1'
-user = 'kccnpcacazyzlp'
-db = 'de4jsjsel5i78g'
-port = 5432
-host = 'ec2-3-225-213-67.compute-1.amazonaws.com'
